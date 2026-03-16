4b335d51202e727e2592fda2e2c157fe99646539
Andrey Zuev <a.zuev@texrus.com>
2026-03-17 02:33:46 +1000

    Edit 3

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
-import { Text, View } from "react-native";
-
+import { Text, View, ImageBackground } from 'react-native';
+const logo = require('../assets/images/react-logo.png');
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+        backgroundColor: 'plum',
+        padding: 60,
+      }}>
+      <Text>
+        Edit app/index.tsx to <Text style={{ color: 'white' }}>edit this screen</Text>.
+      </Text>
+      <ImageBackground style={{ flex: 1 }} source={logo}>
+        <Text>Text</Text>
+      </ImageBackground>
     </View>
   );
 }
