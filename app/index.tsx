1f2a691ba25f8e20256eb5db2806663cfaebe806
Andrey Zuev <a.zuev@texrus.com>
2026-03-18 01:06:56 +1000

    Edit 4

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, ImageBackground } from 'react-native';
+import { Text, View, Image, ScrollView } from 'react-native';
 const logo = require('../assets/images/react-logo.png');
 export default function Index() {
   return (
@@ -8,12 +8,49 @@ export default function Index() {
         backgroundColor: 'plum',
         padding: 60,
       }}>
-      <Text>
-        Edit app/index.tsx to <Text style={{ color: 'white' }}>edit this screen</Text>.
-      </Text>
-      <ImageBackground style={{ flex: 1 }} source={logo}>
-        <Text>Text</Text>
-      </ImageBackground>
+      <ScrollView>
+        <Image style={{ width: 300, height: 300 }} source={logo} />
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum nihil laboriosam similique,
+          quisquam amet fugit tenetur consequatur dolorum laborum vel nemo expedita quasi ipsam
+          sapiente velit ipsa dolore corporis. Similique blanditiis, tempora omnis inventore
+          adipisci sunt ullam ut consequatur! Reprehenderit, a explicabo! Facilis cupiditate quaerat
+          aspernatur totam maxime tenetur, iure error incidunt, nisi iste atque illum molestiae
+          aperiam, odio quae sapiente ab quidem consequuntur temporibus placeat adipisci numquam cum
+          recusandae. Dignissimos quis dolorem, inventore reiciendis, accusamus quas doloremque
+          aspernatur voluptate nobis ab laboriosam quidem eos iste deleniti accusantium molestias
+          veniam. Odit, voluptas assumenda aperiam rem voluptate ullam necessitatibus. Adipisci,
+          rerum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum nihil laboriosam
+          similique, quisquam amet fugit tenetur consequatur dolorum laborum vel nemo expedita quasi
+          ipsam sapiente velit ipsa dolore corporis. Similique blanditiis, tempora omnis inventore
+          adipisci sunt ullam ut consequatur! Reprehenderit, a explicabo! Facilis cupiditate quaerat
+          aspernatur totam maxime tenetur, iure error incidunt, nisi iste atque illum molestiae
+          aperiam, odio quae sapiente ab quidem consequuntur temporibus placeat adipisci numquam cum
+          recusandae. Dignissimos quis dolorem, inventore reiciendis, accusamus quas doloremque
+          aspernatur voluptate nobis ab laboriosam quidem eos iste deleniti accusantium molestias
+          veniam. Odit, voluptas assumenda aperiam rem voluptate ullam necessitatibus. Adipisci,
+          rerum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum nihil laboriosam
+          similique, quisquam amet fugit tenetur consequatur dolorum laborum vel nemo expedita quasi
+          ipsam sapiente velit ipsa dolore corporis. Similique blanditiis, tempora omnis inventore
+          adipisci sunt ullam ut consequatur! Reprehenderit, a explicabo! Facilis cupiditate quaerat
+          aspernatur totam maxime tenetur, iure error incidunt, nisi iste atque illum molestiae
+          aperiam, odio quae sapiente ab quidem consequuntur temporibus placeat adipisci numquam cum
+          recusandae. Dignissimos quis dolorem, inventore reiciendis, accusamus quas doloremque
+          aspernatur voluptate nobis ab laboriosam quidem eos iste deleniti accusantium molestias
+          veniam. Odit, voluptas assumenda aperiam rem voluptate ullam necessitatibus. Adipisci,
+          rerum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum nihil laboriosam
+          similique, quisquam amet fugit tenetur consequatur dolorum laborum vel nemo expedita quasi
+          ipsam sapiente velit ipsa dolore corporis. Similique blanditiis, tempora omnis inventore
+          adipisci sunt ullam ut consequatur! Reprehenderit, a explicabo! Facilis cupiditate quaerat
+          aspernatur totam maxime tenetur, iure error incidunt, nisi iste atque illum molestiae
+          aperiam, odio quae sapiente ab quidem consequuntur temporibus placeat adipisci numquam cum
+          recusandae. Dignissimos quis dolorem, inventore reiciendis, accusamus quas doloremque
+          aspernatur voluptate nobis ab laboriosam quidem eos iste deleniti accusantium molestias
+          veniam. Odit, voluptas assumenda aperiam rem voluptate ullam necessitatibus. Adipisci,
+          rerum.
+        </Text>
+        <Image style={{ width: 300, height: 300 }} source={logo} />
+      </ScrollView>
     </View>
   );
 }
