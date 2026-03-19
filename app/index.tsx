973201b45cce0c05892979962434a851447869be
Andrey Zuev <a.zuev@texrus.com>
2026-03-19 16:21:07 +1000

    Edit 8

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,5 @@
-import { useState } from 'react';
-import { Text, View, Image, Button, Pressable, Modal } from 'react-native';
-const logo = require('../assets/images/react-logo.png');
+import { View, StatusBar } from 'react-native';
 export default function Index() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <View
       style={{
@@ -10,26 +7,7 @@ export default function Index() {
         backgroundColor: 'plum',
         padding: 60,
       }}>
-      <Button title="Press" onPress={() => setIsModalVisible(true)} color="midnightblue" />
-      <Modal
-        animationType="slide"
-        onRequestClose={() => setIsModalVisible(false)}
-        visible={isModalVisible}
-        presentationStyle="pageSheet">
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: 'lightblue',
-            padding: 60,
-          }}>
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident laudantium amet
-            molestiae modi vel repellendus placeat labore soluta quis inventore, et tempore
-            explicabo deleniti incidunt? Dolore magni nihil doloremque cumque.
-          </Text>
-          <Button title="Close" onPress={() => setIsModalVisible(false)} color="midnightblue" />
-        </View>
-      </Modal>
+      <StatusBar barStyle="dark-content" backgroundColor="lightgreen" />
     </View>
   );
 }
