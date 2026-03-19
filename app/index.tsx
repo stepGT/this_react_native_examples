58ab173ad25045028d7d943121d264005517817b
Andrey Zuev <a.zuev@texrus.com>
2026-03-19 16:08:21 +1000

    Edit 7

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,8 @@
-import { Text, View, Image, Button, Pressable } from 'react-native';
+import { useState } from 'react';
+import { Text, View, Image, Button, Pressable, Modal } from 'react-native';
 const logo = require('../assets/images/react-logo.png');
 export default function Index() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <View
       style={{
@@ -8,31 +10,26 @@ export default function Index() {
         backgroundColor: 'plum',
         padding: 60,
       }}>
-      <Button title="Press" onPress={() => console.log('Button onPress')} color="midnightblue" />
-
-      <Pressable
-        onLongPress={() => console.log('Image onLongPress')}
-        onPressIn={() => console.log('Image onPress')}
-        onPressOut={() => console.log('Image onPressOut')}>
-        <Image source={logo} style={{ width: 300, height: 300 }} />
-      </Pressable>
-
-      <Pressable
-        onLongPress={() => console.log('Text onLongPress')}
-        onPressIn={() => console.log('Text onPress')}
-        onPressOut={() => console.log('Text onPressOut')}>
-        <Text>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit quos, quae quasi,
-          deleniti, adipisci nesciunt nam fuga sequi sint eius exercitationem nisi esse libero eum
-          ex inventore. Earum repudiandae optio assumenda soluta quod minima suscipit ut. Neque vero
-          sed minus necessitatibus reprehenderit fugit, nesciunt eveniet delectus accusamus fugiat
-          minima unde, rerum nostrum quas, magnam fuga atque corrupti nihil molestias qui ex a!
-          Inventore corrupti magni, ad est unde dolore, reprehenderit corporis recusandae incidunt
-          pariatur doloremque voluptatem fugit minus libero at a quasi beatae ducimus consequatur
-          dolorum nemo debitis animi? Perspiciatis et a veniam officiis hic sed, ab consectetur quos
-          alias!
-        </Text>
-      </Pressable>
+      <Button title="Press" onPress={() => setIsModalVisible(true)} color="midnightblue" />
+      <Modal
+        animationType="slide"
+        onRequestClose={() => setIsModalVisible(false)}
+        visible={isModalVisible}
+        presentationStyle="pageSheet">
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'lightblue',
+            padding: 60,
+          }}>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident laudantium amet
+            molestiae modi vel repellendus placeat labore soluta quis inventore, et tempore
+            explicabo deleniti incidunt? Dolore magni nihil doloremque cumque.
+          </Text>
+          <Button title="Close" onPress={() => setIsModalVisible(false)} color="midnightblue" />
+        </View>
+      </Modal>
     </View>
   );
 }
