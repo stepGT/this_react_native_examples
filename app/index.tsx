64b7743f4d9e4508951a641a0fd921ca876658aa
Andrey Zuev <a.zuev@texrus.com>
2026-03-19 16:48:24 +1000

    Edit 10

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { View, StatusBar, ActivityIndicator } from 'react-native';
+import { View, Alert, Button } from 'react-native';
 export default function Index() {
   return (
     <View
@@ -7,7 +7,19 @@ export default function Index() {
         backgroundColor: 'plum',
         padding: 60,
       }}>
-      <ActivityIndicator animating={true} color={'midnightblue'} size={'large'} />
+      <Button
+        title="Alert"
+        onPress={() =>
+          Alert.alert(
+            'Invalidate data!',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam recusandae neque molestias laudantium nobis hic voluptate quidem voluptatibus porro odio, quia atque quam adipisci. Minus quas totam similique quae dicta.',
+            [
+              { text: 'Cancel', onPress: () => console.log('Cancel pressed!') },
+              { text: 'Ok', onPress: () => console.log('Ok pressed!') },
+            ],
+          )
+        }
+      />
     </View>
   );
 }
