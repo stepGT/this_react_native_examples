92c0b8ec5f74db9f5c080dd116685eff91bcb62d
Andrey Zuev <a.zuev@texrus.com>
2026-03-19 15:43:19 +1000

    Edit 6

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, Image, ScrollView, Button } from 'react-native';
+import { Text, View, Image, Button, Pressable } from 'react-native';
 const logo = require('../assets/images/react-logo.png');
 export default function Index() {
   return (
@@ -8,7 +8,31 @@ export default function Index() {
         backgroundColor: 'plum',
         padding: 60,
       }}>
-      <Button color="red" onPress={(e) => console.log(e)} title="Press"></Button>
+      <Button title="Press" onPress={() => console.log('Button onPress')} color="midnightblue" />
+
+      <Pressable
+        onLongPress={() => console.log('Image onLongPress')}
+        onPressIn={() => console.log('Image onPress')}
+        onPressOut={() => console.log('Image onPressOut')}>
+        <Image source={logo} style={{ width: 300, height: 300 }} />
+      </Pressable>
+
+      <Pressable
+        onLongPress={() => console.log('Text onLongPress')}
+        onPressIn={() => console.log('Text onPress')}
+        onPressOut={() => console.log('Text onPressOut')}>
+        <Text>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit quos, quae quasi,
+          deleniti, adipisci nesciunt nam fuga sequi sint eius exercitationem nisi esse libero eum
+          ex inventore. Earum repudiandae optio assumenda soluta quod minima suscipit ut. Neque vero
+          sed minus necessitatibus reprehenderit fugit, nesciunt eveniet delectus accusamus fugiat
+          minima unde, rerum nostrum quas, magnam fuga atque corrupti nihil molestias qui ex a!
+          Inventore corrupti magni, ad est unde dolore, reprehenderit corporis recusandae incidunt
+          pariatur doloremque voluptatem fugit minus libero at a quasi beatae ducimus consequatur
+          dolorum nemo debitis animi? Perspiciatis et a veniam officiis hic sed, ab consectetur quos
+          alias!
+        </Text>
+      </Pressable>
     </View>
   );
 }
