64bc1dc8f97d20fdec1f5359ba83a2c8bddf8430
Andrey Zuev <a.zuev@texrus.com>
2026-03-21 22:58:16 +1000

    Edit 21

--- app/index.tsx
+++ app/index.tsx
@@ -4,9 +4,13 @@ import { View, StyleSheet } from 'react-native';
 export default function Index() {
   return (
     <View style={style.container}>
-      <Box style={{ backgroundColor: '#89e890', paddingVertical: 100 }}>Box 1</Box>
-      <Box style={{ backgroundColor: '#919062' }}>Box 2</Box>
-      <Box style={{ backgroundColor: '#23062c' }}>Box 3</Box>
+      <Box style={{ backgroundColor: '#89e890', alignSelf: 'flex-start' }}>Box 1</Box>
+      <Box style={{ backgroundColor: '#919062', alignSelf: 'flex-end' }}>Box 2</Box>
+      <Box style={{ backgroundColor: '#23062c', alignSelf: 'center' }}>Box 3</Box>
+      <Box style={{ backgroundColor: '#301a6d', alignSelf: 'stretch' }}>Box 4</Box>
+      <Box style={{ backgroundColor: '#c5c217', alignSelf: 'auto' }}>Box 5</Box>
+      <Box style={{ backgroundColor: '#171418' }}>Box 6</Box>
+      <Box style={{ backgroundColor: '#b0a3b4' }}>Box 7</Box>
     </View>
   );
 }
@@ -14,8 +18,7 @@ export default function Index() {
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'baseline',
+    alignItems: 'flex-end',
     borderWidth: 6,
     borderColor: 'red',
     marginTop: 64,
