ca0604654e9b24f925349b9e1efd22e433b640b5
Andrey Zuev <a.zuev@texrus.com>
2026-03-27 16:34:40 +1000

    Edit 31

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,36 @@
+import { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, Dimensions } from 'react-native';
 //
 export default function Index() {
+  const [dimensions, setDimensions] = useState({
+    window: Dimensions.get('window'),
+  });
+  useEffect(() => {
+    const subscribtion = Dimensions.addEventListener('change', ({ window }) => {
+      setDimensions({ window });
+    });
+    return () => subscribtion?.remove();
+  });
+
+  const { window } = dimensions;
+  const windowWidth = window.width;
+  const windowHeight = window.height;
+  console.log([windowWidth, windowHeight]);
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Welcome!</Text>
+      <View
+        style={[
+          styles.box,
+          {
+            width: windowWidth > 500 ? '70%' : '90%',
+            height: windowHeight > 600 ? '60%' : '90%',
+          },
+        ]}>
+        <Text style={{ fontSize: windowWidth > 500 ? 50 : 24 }}>Welcome!</Text>
       </View>
     </View>
   );
 }
-
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
-console.log([windowWidth, windowHeight]);
 //
 const styles = StyleSheet.create({
   container: {
@@ -22,13 +40,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   box: {
-    width: windowWidth > 500 ? '70%' : '90%',
-    height: windowHeight > 600 ? '60%' : '90%',
+    //width: windowWidth > 500 ? '70%' : '90%',
+    //height: windowHeight > 600 ? '60%' : '90%',
     backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    fontSize: windowWidth > 500 ? 50 : 24,
-  },
+  // text: {
+  //   fontSize: windowWidth > 500 ? 50 : 24,
+  // },
 });
