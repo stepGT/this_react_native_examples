22eee1e6e674c0ffb9617d3cf102d48bb5353323
Andrey Zuev <a.zuev@texrus.com>
2026-03-27 16:23:03 +1000

    Edit 30

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,34 @@
-import { View, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
 //
 export default function Index() {
-  return <View style={style.container}></View>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.text}>Welcome!</Text>
+      </View>
+    </View>
+  );
 }
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+console.log([windowWidth, windowHeight]);
 //
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'plum',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  box: {
+    width: windowWidth > 500 ? '70%' : '90%',
+    height: windowHeight > 600 ? '60%' : '90%',
+    backgroundColor: 'lightblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: windowWidth > 500 ? 50 : 24,
   },
 });
