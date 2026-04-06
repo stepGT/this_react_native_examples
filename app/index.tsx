85dc8f8c1114899f77a657100168ab3c5baf1e4c
Andrey Zuev <a.zuev@texrus.com>
2026-04-06 18:22:40 +1000

    Edit 44

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import { StatusBar, StyleSheet, TextInput, Text } from 'react-native';
+import { StatusBar, StyleSheet, TextInput, Text, Switch, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 //
 export default function Index() {
   const [username, setUsername] = useState('');
+  const [isDark, setIsDark] = useState(false);
+  console.log(isDark);
   return (
     <SafeAreaProvider style={styles.container}>
       <TextInput
@@ -18,6 +20,15 @@ export default function Index() {
       />
       <TextInput style={styles.input} placeholder="Enter your username" multiline />
       <Text style={styles.text}>My name is {username}</Text>
+      <View style={styles.switchContainer}>
+        <Text style={styles.text}>Dark mode</Text>
+        <Switch
+          value={isDark}
+          onValueChange={() => setIsDark((prev) => !prev)}
+          trackColor={{ false: '#767577', true: 'lightblue' }}
+          thumbColor="#f4f3f4"
+        />
+      </View>
     </SafeAreaProvider>
   );
 }
@@ -41,5 +52,11 @@ const styles = StyleSheet.create({
   multilineText: {
     minHeight: 100,
     textAlignVertical: 'top',
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
   },
 });
