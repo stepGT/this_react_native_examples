598297f3c31617e301c8ffb95765a503b453a215
Andrey Zuev <a.zuev@texrus.com>
2026-04-06 20:34:25 +1000

    Edit 49

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, StatusBar, View, Text, FlatList } from 'react-native';
+import { StyleSheet, StatusBar, View, Text, FlatList, ActivityIndicator } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 //
-
 export interface Post {
   userId: number;
   id: number;
@@ -12,12 +11,14 @@ export interface Post {
 
 export default function Index() {
   const [postList, setPostList] = useState<Post[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
   //
   const fetchData = async (limit = 10) => {
     try {
       const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
       const data = await response.json();
       setPostList(data);
+      setIsLoading(true);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -26,6 +27,15 @@ export default function Index() {
   useEffect(() => {
     fetchData();
   }, []);
+
+  if (isLoading) {
+    return (
+      <SafeAreaProvider style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text>Loading...</Text>
+      </SafeAreaProvider>
+    );
+  }
 
   return (
     <SafeAreaProvider style={styles.container}>
@@ -81,5 +91,12 @@ const styles = StyleSheet.create({
     fontSize: 24,
     textAlign: 'center',
     marginTop: 12,
+  },
+  loadingContainer: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+    paddingTop: StatusBar.currentHeight,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
